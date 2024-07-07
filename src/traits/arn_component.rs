d41ba661b0459aaaab4b85b473746aeeb9578dd4
Roland Rodriguez <roland@govcraft.ai>
2024-07-06 22:28:39 -0600

    refactor(traits)!: remove lifetime parameters
    This commit removes the lifetime parameters from the ArnComponent trait and its implementations. The 'a lifetime parameter is removed from the trait definition and all associated implementations. The as_cow method now returns a Cow<'static, str> instead of Cow<'a, str>. The macro_rules! impl_arn_component macro is updated to reflect these changes.
    
    BREAKING CHANGE: Lifetime parameters are removed from the ArnComponent trait, which might affect implementations relying on the previous lifetime-bound definitions.
    BREAKING CHANGES: You have made changes that may break backward compatibility. According to Semantic Versioning (SemVer), this requires a major version update. Please verify and update your version number accordingly.

--- src/traits/arn_component.rs
+++ src/traits/arn_component.rs
@@ -2,42 +2,42 @@ use crate::{Account, Category, Domain, Part, Parts, Root};
 use std::borrow::Cow;
 
 /// Represents a component of a Arn (Akton Resource Name) that ensures type safety and ordering.
-pub trait ArnComponent<'a> {
+pub trait ArnComponent {
     /// Returns the prefix string that should appear before this component in a Arn.
     fn prefix() -> &'static str;
     /// The type of the next Arn component in the sequence.
     type NextState;
     /// Returns the string representation of this component.
-    fn as_cow(&self) -> Cow<'a, str>;
+    fn as_cow(&self) -> Cow<'static, str>;
 }
 
 macro_rules! impl_arn_component {
     ($type:ty, $prefix:expr, $next:ty) => {
-        impl<'a> ArnComponent<'a> for $type {
+        impl ArnComponent for $type {
             fn prefix() -> &'static str {
                 $prefix
             }
             type NextState = $next;
-            fn as_cow(&self) -> Cow<'a, str> {
+            fn as_cow(&self) -> Cow<'static, str> {
                 self.0.clone()
             }
         }
     };
 }
 
-impl_arn_component!(Domain<'a>, "arn:", Category<'a>);
-impl_arn_component!(Category<'a>, "", Account<'a>);
-impl_arn_component!(Account<'a>, "", Root<'a>);
-impl_arn_component!(Root<'a>, "", Part<'a>);
-impl_arn_component!(Part<'a>, "", Parts<'a>);
+impl_arn_component!(Domain, "arn:", Category);
+impl_arn_component!(Category, "", Account);
+impl_arn_component!(Account, "", Root);
+impl_arn_component!(Root, "", Part);
+impl_arn_component!(Part, "", Parts);
 
 /// Implementation for the `Parts` component of a Arn.
-impl<'a> ArnComponent<'a> for Parts<'a> {
+impl ArnComponent for Parts {
     fn prefix() -> &'static str {
         ":"
     }
-    type NextState = Parts<'a>;
-    fn as_cow(&self) -> Cow<'a, str> {
+    type NextState = Parts;
+    fn as_cow(&self) -> Cow<'static, str> {
         Cow::Owned(self.to_string())
     }
 }
