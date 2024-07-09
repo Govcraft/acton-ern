8ebea9578d3b6b326cc6742b1a9d054d87a99016
Roland Rodriguez <roland@govcraft.ai>
2024-07-08 21:01:01 -0600

    refactor(traits)!: rename ArnComponent to EidComponent
    Renames the trait ArnComponent to EidComponent across the codebase. Updates all relevant macro rules and implementations to reflect this change. Adds an additional import for IdType. Updates specific implementations for Root and Account to conform to the new EidComponent trait.
    
    BREAKING CHANGE: Renaming ArnComponent to EidComponent will require updates in any code that implements or uses this trait.
    BREAKING CHANGES: You have made changes that may break backward compatibility. According to Semantic Versioning (SemVer), this requires a major version update. Please verify and update your version number accordingly.

--- src/traits/eid_component.rs
+++ src/traits/eid_component.rs
@@ -1,8 +1,9 @@
-use crate::{Account, Category, Domain, Part, Parts, Root};
 use std::borrow::Cow;
 
+use crate::{Account, Category, Domain, IdType, Part, Parts, Root};
+
 /// Represents a component of a Ein (Akton Resource Name) that ensures type safety and ordering.
-pub trait ArnComponent {
+pub trait EidComponent {
     /// Returns the prefix string that should appear before this component in a Ein.
     fn prefix() -> &'static str;
     /// The type of the next Ein component in the sequence.
@@ -11,9 +12,9 @@ pub trait ArnComponent {
     fn as_cow(&self) -> Cow<'static, str>;
 }
 
-macro_rules! impl_arn_component {
+macro_rules! impl_eid_component {
     ($type:ty, $prefix:expr, $next:ty) => {
-        impl ArnComponent for $type {
+        impl EidComponent for $type {
             fn prefix() -> &'static str {
                 $prefix
             }
@@ -24,15 +25,31 @@ macro_rules! impl_arn_component {
         }
     };
 }
+impl<T: IdType + Clone + PartialEq> EidComponent for Root<T> {
+    fn prefix() -> &'static str {
+        ""
+    }
+    type NextState = Part;
+    fn as_cow(&self) -> Cow<'static, str> {
+        self.name.clone()
+    }
+}
+impl EidComponent for Account {
+    fn prefix() -> &'static str {
+        ""
+    }
+    type NextState = Root;
+    fn as_cow(&self) -> Cow<'static, str> {
+        self.0.clone()
+    }
+}
 
-impl_arn_component!(Domain, "arn:", Category);
-impl_arn_component!(Category, "", Account);
-impl_arn_component!(Account, "", Root);
-impl_arn_component!(Root, "", Part);
-impl_arn_component!(Part, "", Parts);
+impl_eid_component!(Domain, "eid:", Category);
+impl_eid_component!(Category, "", Account);
+impl_eid_component!(Part, "", Parts);
 
 /// Implementation for the `Parts` component of a Ein.
-impl ArnComponent for Parts {
+impl EidComponent for Parts {
     fn prefix() -> &'static str {
         ":"
     }
