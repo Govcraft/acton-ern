41e671330d83d21d4d2a6462d711d31ce2447d38
Roland Rodriguez <roland@govcraft.ai>
2024-07-09 01:22:30 -0600

    refactor(traits): remove UserDefined ID type
    Remove the UserDefined struct and its implementation of the IdType trait. This includes the removal of the generate_id function that created v8 UUIDs using user-defined data.

--- src/traits/id_type.rs
+++ src/traits/id_type.rs
@@ -4,15 +4,6 @@ pub trait IdType {
     fn generate_id(value: &str) -> Uuid;
 }
 
-// Implement the trait for each ID version with user-friendly names
-#[derive(Debug, Clone, PartialEq)]
-pub struct Random;
-
-impl IdType for Random {
-    fn generate_id(_: &str) -> Uuid {
-        Uuid::new_v4()
-    }
-}
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct SHA1Name;
@@ -38,17 +29,5 @@ pub struct UnixTime;
 impl IdType for UnixTime {
     fn generate_id(_: &str) -> Uuid {
         Uuid::now_v7()
-    }
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub struct UserDefined;
-
-impl IdType for UserDefined {
-    fn generate_id(value: &str) -> Uuid {
-        // For v8 UUIDs, the user-defined data should be provided
-        // Here, we use a simple example of generating a UUID from a fixed namespace
-        // Adjust this logic based on your specific use case
-        Uuid::new_v8(<[u8; 16]>::try_from(value.as_bytes()).unwrap())
     }
 }
