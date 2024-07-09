3f7adf5e5dca5be3cc55093d740785f76a2304a3
Roland Rodriguez <roland@govcraft.ai>
2024-07-08 21:01:05 -0600

    refactor(model)!: generalize Root struct
    This commit refactors the Root struct to be generic over an IdType trait. This allows for more flexibility in specifying the type of ID used within the Root struct. Changes include:
    
    - Updating the Root struct to accept a generic parameter T that implements IdType, Clone, and PartialEq.
    - Modifying methods like as_str, into_owned, new, and fmt to accommodate the generic type.
    - Updating the Default and FromStr implementations for Root to handle the generic type.
    - Adjusting test cases to use the new generic Root struct.
    
    BREAKING CHANGE: The Root struct now requires a generic parameter T that implements IdType, Clone, and PartialEq.
    BREAKING CHANGES: You have made changes that may break backward compatibility. According to Semantic Versioning (SemVer), this requires a major version update. Please verify and update your version number accordingly.

--- src/model/root.rs
+++ src/model/root.rs
@@ -1,59 +1,64 @@
-use crate::errors::ArnError;
-use derive_more::{AsRef, From, Into};
 use std::borrow::Cow;
 use std::fmt;
+
+use derive_more::{AsRef, From, Into};
 use type_safe_id::{DynamicType, TypeSafeId};
 
-#[derive(AsRef, From, Into, Eq, Debug, PartialEq, Clone, Hash)]
-pub struct Root(pub(crate) Cow<'static, str>);
+use crate::errors::ArnError;
+use crate::{IdType, UnixTime};
 
-impl Root {
+#[derive(AsRef, From, Into, Eq, Debug, PartialEq, Clone, Hash)]
+pub struct Root<T: IdType + Clone + PartialEq = UnixTime> {
+    pub(crate) name: Cow<'static, str>,
+    marker: std::marker::PhantomData<T>,
+}
+
+impl<T: IdType + Clone + PartialEq> Root<T> {
     pub fn as_str(&self) -> &str {
-        &self.0
+        &self.name
     }
 
-    pub fn into_owned(self) -> Root {
-        Root(Cow::Owned(self.0.into_owned()))
+    pub fn into_owned(self) -> Root<T> {
+        Root { name: Cow::Owned(self.name.into_owned()), marker: Default::default() }
     }
 
     pub fn new(value: impl Into<Cow<'static, str>>) -> Result<Self, ArnError> {
         let value = value.into();
         let value = if value.is_empty() {
-            let val = AKTON;
-            TypeSafeId::from_type_and_uuid(DynamicType::new(val)?, uuid::Uuid::now_v7())
-                .to_string()
+            let val = ACTON;
+            TypeSafeId::from_type_and_uuid(DynamicType::new(val)?, T::generate_id(val)).to_string()
         } else {
-            TypeSafeId::from_type_and_uuid(DynamicType::new(&value)?, uuid::Uuid::now_v7())
+            TypeSafeId::from_type_and_uuid(DynamicType::new(&value)?, T::generate_id(value.to_string().as_str()))
                 .to_string()
         };
-        Ok(Root(Cow::from(value)))
+        Ok(Root { name: Cow::from(value), marker: Default::default() })
     }
 }
 
-impl Default for Root {
+impl<T:IdType+Clone+PartialEq> Default for Root<T> {
     fn default() -> Self {
         Root::new("").expect("Couldn't create default Akton ARN")
     }
 }
 
-impl fmt::Display for Root {
+impl<T:IdType+Clone+PartialEq> fmt::Display for Root<T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let id = &self.0;
+        let id = &self.name;
         write!(f, "{id}")
     }
 }
-const AKTON: &str = "akton";
+const ACTON: &str = "akton";
 
-impl std::str::FromStr for Root {
+impl<T:IdType+Clone+PartialEq> std::str::FromStr for Root<T> {
     type Err = ArnError;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         Root::new(s.to_string())
     }
 }
 
-impl From<Root> for String {
-    fn from(root: Root) -> Self {
-        root.0.into_owned()
+impl<T:IdType+Clone+PartialEq> From<Root<T>> for String {
+    fn from(root: Root<T>) -> Self {
+        root.name.into_owned()
     }
 }
 #[cfg(test)]
@@ -62,40 +67,41 @@ mod tests {
 
     #[test]
     fn test_root_creation() {
-        let root = Root::new("test").unwrap();
+        let root: Root<UnixTime> = Root::new("test").unwrap();
         assert!(root.as_str().starts_with("test"));
     }
 
     #[test]
     fn test_root_default() {
-        let root = Root::default();
+        let root: Root<UnixTime> = Root::default();
         assert!(root.as_str().starts_with("akton"));
     }
 
     #[test]
     fn test_root_display() {
-        let root = Root::new("example").unwrap();
+        let root: Root<UnixTime> = Root::new("example").unwrap();
         assert!(format!("{}", root).starts_with("example"));
     }
 
     #[test]
     fn test_root_from_str() {
-        let root: Root = "test".parse().unwrap();
+        let root: Root<UnixTime> = "test".parse().unwrap();
         assert!(root.as_str().starts_with("test"));
     }
 
     #[test]
-    fn test_root_equality() {
-        let root1 = Root::new("test");
-        let root2 = Root::new("test");
-        let root3 = Root::new("other");
+    fn test_root_equality() -> Result<(), ArnError> {
+        let root1: Root<UnixTime> = Root::new("test")?;
+        let root2: Root<UnixTime> = Root::new("test")?;
+        let root3: Root<UnixTime> = Root::new("other")?;
         assert_ne!(root1, root2);
         assert_ne!(root1, root3);
+        Ok(())
     }
 
     #[test]
     fn test_root_into_string() {
-        let root = Root::new("test").unwrap();
+        let root: Root<UnixTime> = Root::new("test").unwrap();
         let string: String = root.into();
         assert!(string.starts_with("test"));
     }
